77df1c6edfc8595624602341e90d7157e84c7dd4
tyrannikal <101421596+tyrannikal@users.noreply.github.com>
2026-03-15 14:22:44 -0400

    get tui running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use ratatui::{DefaultTerminal, Frame};
+use std::io;
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    ratatui::run(app)?;
+    Ok(())
+}
+
+fn app(terminal: &mut DefaultTerminal) -> io::Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if crossterm::event::read()?.is_key_press() {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("press q to quit", frame.area());
 }
